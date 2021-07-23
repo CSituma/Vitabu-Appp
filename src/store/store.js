4f5bc10645b78e626285beaf1554a064ac464210
Clara Situma <cnsituma@gmail.com>
2021-07-24 02:23:27 +0300

    Edit Home page

--- src/store/store.js
+++ src/store/store.js
@@ -6,7 +6,7 @@ const middleware =[thunk];
 const store = createStore(
   allReducers,
   compose(applyMiddleware(...middleware),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+ // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   ));
 
 
